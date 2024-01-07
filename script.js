dcc41a45692493f7eb8d9fb3ed391360bd895728
Ruan <rugcosta1234@gmail.com>
2024-01-07 12:09:10 -0300

    resolvendo as issues do html e finalizando o a logica do javascript

--- script.js
+++ script.js
@@ -1,24 +1,51 @@
 const successContainerMain = document.getElementById("success-container-main");
 const mainContainer = document.getElementById("main-container");
-const email = document.getElementById("email").value;
-const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
+const email = document.getElementById("email");
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 const messageError = document.getElementById("error-message");
 
-function formValid() {
+
+// Codigo antigo funcional 
+
+// email.addEventListener('change', (e) => e.target.value);
+
+// function formValid() {
+//   //Exibir mensagem de erro se o e-mail estiver vazio ou não passar na verificação da expressão regular
+//   if (email.value === "" || !emailRegex.test(email.value.trim())) {
+//     console.log("Valor do e-mail: ", email.value)
+//     messageError.style.display = "block";
+//     successContainerMain.style.display = "none";
+//     // Exibir mensagem de sucesso se o e-mail for válido
+//   } else {
+//     messageError.style.display = "none";
+//     successContainerMain.style.display = "block";
+//     mainContainer.style.display = "none";
+//   }
+// }
+// //Exibir mensagem de sucesso e redefinir o formulário para um novo envio
+// function successForm() {
+//   messageError.style.display = "none";
+//   successContainerMain.style.display = "none";
+//   mainContainer.style.display = "block";
+// }
+
+document.getElementById("myForm").addEventListener("submit", function (event) {
+  //Evitar que o formulário seja enviado imediatamente
+  event.preventDefault();
   //Exibir mensagem de erro se o e-mail estiver vazio ou não passar na verificação da expressão regular
-  if (email === "" || !emailRegex.test(email)) {
+  if (email.value === " " || !emailRegex.test(email.value)) {
     messageError.style.display = "block";
     successContainerMain.style.display = "none";
-    // Exibir mensagem de sucesso se o e-mail for válido
   } else {
     messageError.style.display = "none";
     successContainerMain.style.display = "block";
     mainContainer.style.display = "none";
   }
-}
+});
+
 //Exibir mensagem de sucesso e redefinir o formulário para um novo envio
-function successForm() {
-  messageError.style.display = "block";
+document.getElementById("myButton").addEventListener("click", function () {
+  messageError.style.display = "none";
   successContainerMain.style.display = "none";
   mainContainer.style.display = "block";
-}
+});
